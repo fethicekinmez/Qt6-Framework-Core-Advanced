e8a23ef47bfaeff655d8fef4f49207e272b9d2b9
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-05-06 13:08:20 +0300

    represent and manipulate IP addresses in both IPv4 and IPv6 formats.

---
+++ 04-Networking/01-QHostAddress/main.cpp
@@ -0,0 +1,41 @@
+#include <QCoreApplication>
+
+#include <QHostAddress>
+#include <QNetworkInterface>
+#include <QAbstractSocket>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QList<QHostAddress> addressList = QNetworkInterface::allAddresses();
+
+    for(int i=0; i<addressList.count(); i++){
+
+        QHostAddress address = addressList.at(i);
+        qInfo() << address.toString();
+
+        qInfo() << "\t LoopBack: " << address.isLoopback(); // true if it is a loobback address, meaning information never goes out to the internet.
+        qInfo() << "\t Mulitcast: " << address.isMulticast(); // It's a way to send data to multiple recipients at once
+
+        switch(address.protocol()){
+        case QAbstractSocket::UnknownNetworkLayerProtocol:
+            qInfo() << "\t Protocol: Unknown";
+            break;
+        case QAbstractSocket::AnyIPProtocol:
+            qInfo() << "\t Protocol: Any";
+            break;
+        case QAbstractSocket::IPv6Protocol:
+            qInfo() << "\t Protocol: IPv6";
+            break;
+        case QAbstractSocket::IPv4Protocol:
+            qInfo() << "\t Protocol: IPv4";
+            break;
+
+        }
+
+    }
+
+    return a.exec();
+}
